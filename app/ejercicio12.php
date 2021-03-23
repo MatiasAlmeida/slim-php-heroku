2542f1e640e992cfd7bf2fca1f43572a859a7222
MatiasAlmeida <mati_sharck@hotmail.com>
2021-03-23 20:11:16 -0300

    ejercicio12

---
+++ app/ejercicio12.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Almeida Matias
+Aplicación No 12 (Invertir palabra)
+Realizar el desarrollo de una función que reciba un Array de caracteres y que invierta el orden
+de las letras del Array.
+Ejemplo: Se recibe la palabra “HOLA” y luego queda “ALOH”.
+*/
+$palabra = "HOLA";
+$palabraInvertida = $palabra;
+
+print("Palabra: $palabra<br>Palabra invertida: $palabraInvertida");
+
+function invertir_palabra($string) {
+    return strrev($string);
+}
+
+?>
