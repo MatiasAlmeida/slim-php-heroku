2ae99a3d847fdccdc1121ced1939eda7d9e584af
MatiasAlmeida <mati_sharck@hotmail.com>
2021-04-18 11:36:12 -0300

    ejercicio15

--- app/ejercicio15/Test.php
+++ app/ejercicio15/Test.php
@@ -1,10 +1,10 @@
 <?php
 
 include './Rectangulo.php';
-include './Triangulo.php';
+//include './Triangulo.php';
 
 $newRectangle = new Rectangulo(4, 5);
-$newTriangle = new Triangulo(2, 5);
+//$newTriangle = new Triangulo(2, 5);
 
 $newRectangle->Dibujar();
 $newRectangle->__toString();
